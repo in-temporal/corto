eec7c2cb32ed98553712ce61e409e6fa5b0ded22
alessandrovaru <alessandrovaru@gmail.com>
2022-04-09 00:00:55 -0400

    GA TAG

--- src/components/Intemporal/index.jsx
+++ src/components/Intemporal/index.jsx
@@ -7,9 +7,9 @@ const index = ({ logo }) => {
       <div className="About-img-absolute">
         <img src={logo} className="Loader-logo" alt="logo" />
       </div>
-      <h2>¿Qué es In Temporal?</h2>
+      <h6>¿Qué es In Temporal?</h6>
       <Container>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/gaafiHeJ88w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe className='About-iframe' width="560" height="315" src="https://www.youtube.com/embed/gaafiHeJ88w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </Container>
       <Container>
         <p>Un par de adolescentes irrumpen en una mansión abandonada. Al explorar el interior, descubren que la casa se distorsiona y el chico queda atrapado en una habitación. Mientras la chica intenta ayudarlo a salir, observa que el tiempo cambia y que en la casa habitan sus versiones futuras.</p>
